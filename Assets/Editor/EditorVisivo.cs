8df5e05bcae6bf588cd10cd090f78f5ed18238a8
loryplayer <54544727+loryplayer@users.noreply.github.com>
2021-09-19 17:01:59 +0200

    Add files via upload

---
+++ Assets/Editor/EditorVisivo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public partial class EditorVisivo : UnityEditor.Editor {
+
+    void OnSceneGUI() {
+        VisualeEn fow = (VisualeEn)target;
+        Handles.color = Color.white;
+        var position = fow.transform.position;
+        Handles.DrawWireArc (position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+        Vector3 viewAngleA = fow.DirFromAngle (-fow.viewAngle / 2, false);
+        Vector3 viewAngleB = fow.DirFromAngle (fow.viewAngle / 2, false);
+        Handles.DrawLine (position, position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine (position, position + viewAngleB * fow.viewRadius);
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in VisualeEn.visibleTargets) 
+        {
+            if(visibleTarget!=null)
+                Handles.DrawLine (fow.transform.position, visibleTarget.position);
+        }
+    }
+
+}
